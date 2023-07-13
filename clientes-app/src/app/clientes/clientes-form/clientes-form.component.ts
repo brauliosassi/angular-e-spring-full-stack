c09b1068a98f80dd6b5797a59b0491a0941a5e1e
Braulio <braulio.sassi@gmail.com>
2023-07-13 14:31:14 -0300

    71 à 75 - Atualiazar cadastro

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { Cliente } from '../cliente';
-import {} from './../../clientes.service';
 import { ClientesService } from '../../clientes.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
@@ -14,15 +15,50 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   success: boolean = false;
   errors: String[];
+  id: number;
 
-  constructor(private service: ClientesService) {
-    this.cliente = new Cliente();
-  }
+  constructor(
+    private service: ClientesService,
+    private router : Router,
+    private activatedRoute : ActivatedRoute
+    )
+    {
+      this.cliente = new Cliente();
+    }
 
   ngOnInit(): void {
+    let params : Observable<Params> = this.activatedRoute.params
+    params.subscribe( urlParams => {
+      this.id =  urlParams['id'];
+      if(this.id) {
+        this.service
+        .getClienteById(this.id)
+        .subscribe(
+          response => this.cliente = response,
+          errorResponse => this.cliente = new Cliente()
+        )
+      }
+    })
+  }
+
+  voltarParaListagem(){
+    this.router.navigate(['/clientes-lista'])
   }
 
   onSubmit() {
+    if(this.id){
+      this.service
+        .atualizar(this.cliente)
+        .subscribe(response => {
+          this.success = true;
+          this.errors = null;
+        }, errorResponse => {
+          this.errors = ['Erro ao atualizar cliente.']
+        })
+
+    } else {
+
+    }
     this.service
         .salvar( this.cliente)
         .subscribe( response => {
